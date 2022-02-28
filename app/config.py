289b8b0dae50c63330af6d6047616b63a45472d6
Demitri Grant <demitri.grant@gmail.com>
2022-02-27 23:50:48 -0500

    final push

--- app/config.py
+++ app/config.py
@@ -9,7 +9,7 @@ class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY', 'Som3$ec5etK*y')
     ADMIN_USERNAME = os.environ.get('ADMIN_USERNAME', 'admin')
     ADMIN_PASSWORD = os.environ.get('ADMIN_PASSWORD', 'Password123')
-    UPLOAD_FOLDER = os.environ.get('UPLOAD_FOLDER', './uploads')
+    UPLOAD_FOLDER = os.environ.get('UPLOAD_FOLDER')
 
 
 
